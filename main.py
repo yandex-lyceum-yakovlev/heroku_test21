6f1497c07b3df66e583c67f3a9fa72b9b3b25546
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2021-04-06 18:24:26 +0400

    port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -14,4 +15,5 @@ def index(lst):
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
